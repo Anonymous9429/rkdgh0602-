afd0355fab661057630be57fc85e0b1191350471
parkkangho <burgerking@handong.ac.kr>
2022-09-17 21:14:52 +0900

    create file

--- src/main/java/handong/csee/project/WordCRUD.java
+++ src/main/java/handong/csee/project/WordCRUD.java
@@ -94,6 +94,19 @@ public class WordCRUD implements ICRUD {
         return idlist;
     }
 
+    public void listAll(int level){
+        int j= 0;
+        System.out.println("--------------------------------");
+        for (int i = 0; i < list.size(); i++) {
+            int ilevel = list.get(i).getLevel();
+            if (ilevel != level) continue;
+            System.out.print((j + 1) + " ");
+            System.out.println(list.get(i).toString());
+            j++;
+        }
+        System.out.println("--------------------------------");
+
+    }
     public void updateItem() {
         System.out.print("==> 수정할 단어 검색: ");
         String keyword = s.next();
